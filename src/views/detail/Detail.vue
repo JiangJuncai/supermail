4de690816002d9211c4f8c2b5a2b88d608ae4403
stans <834410151@qq.com>
2019-08-17 23:07:09 +0800

    detail data&swiper

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="detail">
     <detail-nav-bar />
+    <detail-swiper :top-images="topImages" />
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar'
+import DetailSwiper from './childComps/DetailSwiper'
 
-import { getDetail } from 'network/detail'
+import { getDetail, Goods } from 'network/detail'
 
 export default {
   name: 'Detail',
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper
   },
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: [],
+      goods: null
     }
   },
   created() {
@@ -25,7 +30,15 @@ export default {
 
     // 2、根据iid请求详情数据
     getDetail(this.iid).then(res => {
-      console.log(res)
+      const data = res.result
+      // 1、获取顶部的图片轮播数据
+      this.topImages = data.itemInfo.topImages
+      // 2、获取商品信息
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      )
     })
   }
 }

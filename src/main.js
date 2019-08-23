4cb919f7f0ed6dac5c405972bb20a69cd2a17f80
stans <834410151@qq.com>
2019-08-23 12:39:23 +0800

    fastclick lazyload

--- src/main.js
+++ src/main.js
@@ -3,12 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
 import toast from 'components/common/toast'
 
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 
 Vue.use(toast)
+
+// 解决移动端300ms的延迟
+FastClick.attach(document.body)
+
+// 使用懒加载
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
